a6d7b519898deb59a7c608cb799678e8f565687c
John Leward Escote <jleward.escote17@gmail.com>
2026-07-09 16:58:29 +0800

    feat(public): add initial website pages
    Signed-off-by: John Leward Escote <jleward.escote17@gmail.com>

---
+++ app/Http/Controllers/Public/ReservationRequestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+
+use App\Http\Controllers\Controller;
+
+class ReservationRequestController extends Controller
+{
+    public function create()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
+use App\Http\Controllers\Public\BanquetHallController;
+use App\Http\Controllers\Public\ContactController;
+use App\Http\Controllers\Public\GalleryController;
+use App\Http\Controllers\Public\HomeController;
+use App\Http\Controllers\Public\MenuController;
+use App\Http\Controllers\Public\OrderInquiryController;
+use App\Http\Controllers\Public\ReservationRequestController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', HomeController::class)->name('home');
+Route::get('/menu', MenuController::class)->name('menu');
+Route::get('/gallery', GalleryController::class)->name('gallery');
+Route::get('/banquet-hall', BanquetHallController::class)->name('banquet-hall');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
-});
+Route::get('/reservation-request', [ReservationRequestController::class, 'create'])
+    ->name('reservation-request.create');
 
-require __DIR__.'/settings.php';
+Route::get('/order-inquiry', [OrderInquiryController::class, 'create'])
+    ->name('order-inquiry.create');
+
+Route::get('/contact', [ContactController::class, 'create'])
+    ->name('contact.create');
