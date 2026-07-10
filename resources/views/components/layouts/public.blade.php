c2e61324396cc8e981626a202e7aa36384000d3a
John Leward Escote <jleward.escote17@gmail.com>
2026-07-10 15:05:51 +0800

    feat(accessibility): add keyboard skip navigation

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -9,10 +9,17 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-h-screen bg-stone-950 text-stone-100 antialiased">
+    <a
+        href="#main-content"
+        class="fixed left-4 top-4 z-[100] -translate-y-24 rounded-md bg-amber-300 px-4 py-2 font-semibold text-stone-950 transition focus:translate-y-0 focus:outline-none focus:ring-2 focus:ring-white"
+    >
+        Skip to main content
+    </a>
+
     <div class="min-h-screen">
         <x-public.header />
 
-        <main>
+        <main id="main-content" tabindex="-1">
             {{ $slot }}
         </main>
 
