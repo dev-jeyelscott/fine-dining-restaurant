95291cf0afd01b2e245eb43159a96d7db68d915e
John Leward Escote <jleward.escote17@gmail.com>
2026-07-10 15:16:22 +0800

    revert(seo): remove optional crawl endpoints

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\PublicSite\HomeController;
 use App\Http\Controllers\PublicSite\MenuController;
 use App\Http\Controllers\PublicSite\OrderInquiryController;
 use App\Http\Controllers\PublicSite\ReservationRequestController;
-use App\Http\Controllers\PublicSite\SeoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -26,9 +25,6 @@ Route::get('/order-inquiry', [OrderInquiryController::class, 'create'])
 
 Route::get('/contact', [ContactController::class, 'create'])
     ->name('contact.create');
-
-Route::get('/robots.txt', [SeoController::class, 'robots'])->name('robots');
-Route::get('/sitemap.xml', [SeoController::class, 'sitemap'])->name('sitemap');
 
 Route::middleware('throttle:public-forms')->group(function (): void {
     Route::post('/reservation-requests', [ReservationRequestController::class, 'store'])
