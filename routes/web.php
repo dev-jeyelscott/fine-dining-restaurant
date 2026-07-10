fd7d6ba2c5963c25a213510cdbb9ca98f819386a
John Leward Escote <jleward.escote17@gmail.com>
2026-07-10 15:14:51 +0800

    refactor(seo): route crawl endpoints through controller

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PublicSite\HomeController;
 use App\Http\Controllers\PublicSite\MenuController;
 use App\Http\Controllers\PublicSite\OrderInquiryController;
 use App\Http\Controllers\PublicSite\ReservationRequestController;
-use Illuminate\Http\Response;
+use App\Http\Controllers\PublicSite\SeoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -27,47 +27,8 @@ Route::get('/order-inquiry', [OrderInquiryController::class, 'create'])
 Route::get('/contact', [ContactController::class, 'create'])
     ->name('contact.create');
 
-Route::get('/robots.txt', function (): Response {
-    $content = implode("\n", [
-        'User-agent: *',
-        'Allow: /',
-        'Disallow: /admin',
-        'Sitemap: '.route('sitemap'),
-        '',
-    ]);
-
-    return new Response($content, 200, [
-        'Content-Type' => 'text/plain; charset=UTF-8',
-    ]);
-})->name('robots');
-
-Route::get('/sitemap.xml', function (): Response {
-    $urls = [
-        route('home'),
-        route('menu'),
-        route('reservation-request.create'),
-        route('order-inquiry.create'),
-        route('gallery'),
-        route('banquet-hall'),
-        route('contact.create'),
-    ];
-
-    $entries = implode("\n", array_map(
-        static fn (string $url): string => '    <url><loc>'.e($url).'</loc></url>',
-        $urls,
-    ));
-
-    $content = <<<XML
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-{$entries}
-</urlset>
-XML;
-
-    return new Response($content, 200, [
-        'Content-Type' => 'application/xml; charset=UTF-8',
-    ]);
-})->name('sitemap');
+Route::get('/robots.txt', [SeoController::class, 'robots'])->name('robots');
+Route::get('/sitemap.xml', [SeoController::class, 'sitemap'])->name('sitemap');
 
 Route::middleware('throttle:public-forms')->group(function (): void {
     Route::post('/reservation-requests', [ReservationRequestController::class, 'store'])
