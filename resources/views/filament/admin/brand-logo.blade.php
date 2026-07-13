b77e8ed1fffbec021be7232674f3923efd8ac1d1
John Leward Escote <jleward.escote17@gmail.com>
2026-07-13 11:57:41 +0800

    refactor(admin): reuse context-aware brand markup

--- resources/views/filament/admin/brand-logo.blade.php
+++ resources/views/filament/admin/brand-logo.blade.php
@@ -1,14 +1,1 @@
-@php
-    $restaurantName = (string) config('app.name');
-@endphp
-
-<span class="admin-brand-lockup">
-    <span class="admin-brand-lockup-name">
-        {{ $restaurantName }}
-    </span>
-
-    <span
-        class="admin-brand-lockup-mark"
-        aria-hidden="true"
-    ></span>
-</span>
+@include('filament.admin.brand')
